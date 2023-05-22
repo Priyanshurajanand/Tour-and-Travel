4b5b74451b00b26a9c5592d895b30ab765feb6a9
Priyanshurajanand <panand@iiitmanipur.ac.in>
2023-05-22 21:29:04 +0530

    Login and Register page completed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Container , Row , Col , Form , FormGroup , Button} from 'reactstrap'
+import {Link} from 'react-router-dom'
+import '../styles/login.css'
+
+import registerImg from '../assets/images/register.png'
+import userIcon from '../assets/images/user.png'
 
 const Register = () => {
+
+  const [credentials , setCredentials] = useState({
+    userName:undefined,
+    email: undefined,
+    password: undefined
+  })
+
+  const handleChange = e =>{
+    setCredentials(prev => ({ ...prev , [e.target.id]:e.target.value}));
+  };
+
+  const handleClick = e => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg="8" className='m-auto'>
+            <div className="login_container d-flex justify-content-between">
+              <div className="login_img">
+                <img src={registerImg} alt="" />
+              </div>
+              <div className="login_form">
+                <div className="user">
+                  <img src={userIcon} alt="" />
+                </div>
+                <h2>Register</h2>
+                <Form onSubmit={handleClick}>
+                  <FormGroup>
+                    <input type="text" placeholder='Username' required id='userName' onChange={handleChange} />
+                  </FormGroup>
+                  <FormGroup>
+                    <input type="email" placeholder='Email' required id='email' onChange={handleChange} />
+                  </FormGroup>
+                  <FormGroup>
+                    <input type="password" placeholder='Password' required id='password' onChange={handleChange} />
+                  </FormGroup>
+                  <Button className='btn secondary_btn auth_btn' type='submit'>Create Account</Button>
+                </Form>
+                <p>Already have an account ? <Link to='/login'>Login</Link></p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
