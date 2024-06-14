997c34775b80b31015deddebc7c30d41eaf7c94d
Priyanshu Raj Anand <panand@iiitmanipur.ac.in>
2024-06-14 13:58:06 +0530

    image fixed

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000;
 const corsOptions ={
-    origin:'https://tour-and-travel-nu.vercel.app/', 
+    origin:'https://tour-and-travel-nu.vercel.app/home', 
     // origin: 'http://localhost:3000',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
