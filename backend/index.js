4bcf72875cc31436b5f5f24d9c3b812e9ec0bb13
Priyanshu Raj Anand <panand@iiitmanipur.ac.in>
2024-06-14 14:12:26 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const connect =  async()=>{
 
 //middleware
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use('/api/v1/auth', authRoute );
 app.use('/api/v1/tours', tourRoute );
